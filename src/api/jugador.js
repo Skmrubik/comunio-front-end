15e0967216d654704fc5e5561d2095023391aba5
JORGE\jorge <skmrubik@gmail.com>
2025-09-15 11:49:16 +0200

    cambio de jugadores suplentes

--- src/api/jugador.js
+++ src/api/jugador.js
@@ -17,3 +17,13 @@ export function getPuntosJugador(idJugador) {
     return fetch('http://localhost:8081/getPuntosJugador?idJugador='+idJugador)
         .then((response) => response.json())
 }
+
+export function getJugadoresSuplentesPosicion(idParticipante, posicion) {
+    return fetch('http://localhost:8080/listJugadoresSuplentesPosicion?idParticipante='+idParticipante+'&posicion='+posicion)
+        .then((response) => response.json())
+}
+
+export function cambioJugador(idTitular, idSuplente) {
+    return fetch('http://localhost:8080/cambioJugador?idTitular='+idTitular+'&idSuplente='+idSuplente)
+        .then((response) => response.json())
+}
--- src/components/ContainerJugadores.jsx
+++ src/components/ContainerJugadores.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect} from 'react'
-import { getJugadoresTitulares } from '../api/jugador';
+import { getJugadoresTitulares, getJugadoresSuplentesPosicion, cambioJugador } from '../api/jugador';
 import  svgIconCambio  from '../assets/angle-right.svg?import';
 import  svgBallFootball  from '../assets/ball-football.svg?import';
+import  arrowUp  from '../assets/up-chevron.svg?import';
+import  arrowDown  from '../assets/down-chevron.svg?import';
 import { useEstado } from '../store/estado.js';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,7 +11,10 @@ function ContainerJugadores ({ titulo, jugPropios, idParticipante }) {
 
   const numeroJornadaEstado = useEstado((state) => state.numeroJornada);
   const [titulares, setTitulares] = useState([]);
+  const [suplentes, setSuplentes] = useState([]);
+  const [mostrarSuplentes, setMostrarSuplentes] = useState(false);
   const [jugadoresPropios, setJugadoresPropios] = useState(true);
+  const [jugadorCambioActual, setJugadorCambioActual] =useState(0);
   const obtenerIdParticipante = useEstado((state) => state.getIdParticipanteJugadores);
   const obtenerIdParticipanteEstado = useEstado((state) => state.idParticipanteJugadores);
   const navigate = useNavigate();
@@ -26,8 +31,6 @@ function ContainerJugadores ({ titulo, jugPropios, idParticipante }) {
       });
   }, [obtenerIdParticipanteEstado]);
 
-  
-
   useEffect(() => {
     setJugadoresPropios(idParticipante == obtenerIdParticipante());
     getJugadoresTitulares(obtenerIdParticipanteEstado)
@@ -37,11 +40,45 @@ function ContainerJugadores ({ titulo, jugPropios, idParticipante }) {
       .catch((err) => {
         console.log(err.message);
       });
-  }, [numeroJornadaEstado]);   
+  }, [numeroJornadaEstado]);  
   
-  function goJugador(jugador) {
-    navigate('/jugador', { state: { jugador } });
+  function cambiarJugador(jugador) {
+    if (mostrarSuplentes) {
+      setMostrarSuplentes(false);
+    } else {
+      getJugadoresSuplentesPosicion(obtenerIdParticipanteEstado, jugador.posicion)
+      .then(items => {
+        setSuplentes(items);
+        setJugadorCambioActual(jugador.id_jugador)
+        setMostrarSuplentes(true);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    }
   }
+  
+  function cambiarJugadorSuplente(jugador, suplente) {
+    cambioJugador(jugador, suplente)
+      .then(items => {
+        setMostrarSuplentes(false);
+        getJugadoresTitulares(obtenerIdParticipanteEstado)
+        .then(items => {
+          setTitulares(items);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
+  function goJugador(jugador) {
+    navigate('/jugador', { state: { jugador } });  
+  }
+
   return (
     <div className='container-jugadores'>
         <p className='titulo-clasificacion'>{titulo}</p>
@@ -53,27 +90,57 @@ function ContainerJugadores ({ titulo, jugPropios, idParticipante }) {
           <p className={jugadoresPropios?'header-jugador-puntos-totales-cambio':'header-jugador-puntos-totales'}>P. Totales</p>
         </div>
         {titulares.map((jugador, index) => {
-        return(
-          <div className='jugador' style={{borderLeft: jugador.posicion==1?'5px solid #ffc107':jugador.posicion==2?'5px solid #2196f3':jugador.posicion==3?'5px solid #4caf50':'5px solid #f44336', 
-            borderBottom: index==10?'none':'1px solid #ccc', borderRadius: index==10? '0px 0px 0px 5px':'0px 0px 0px 0px',
-            borderTop: index==0?'1px solid #ccc':'none', color: jugadoresPropios?'black':'dimgray'}}key={index}>
-            <p className='jugador-nombre' onClick={()=> goJugador(jugador)}>{jugador.nombre}</p>
-            <div className='jugador-equipo'>
-              <img  src={'/'+jugador.path_foto+'.png'} style={{width: 25, height: 25, marginTop: 5}}/>
-            </div>
-            <div className={jugadoresPropios?'jugador-puntos-media-cambio':'jugador-puntos-media'} style={{display: 'flex', flexDirection: 'row', justifyContent: 'end'}}>
-                                  {jugador.goles!=0 && [...Array(jugador.goles)].map((_, idx) => (
-                                    <img src={svgBallFootball} style={{height: 15, width: 15, marginTop: 11, marginLeft: 4}}/>
-                                  ))}
-                                  <p className={jugPropios?'jugador-puntos-jornada-cambio':'jugador-puntos-jornada'} style={{color: jugador.puntos_jornada<0?'#f44336':jugador.puntos_jornada<1?'#78909c'
-                                    :jugador.puntos_jornada<5?'#ff9800':jugador.puntos_jornada<10?'#4caf50':'#2196f3', fontWeight: 600}}>{jugador.puntos_jornada}</p>
-                                </div>
-            <p className={jugadoresPropios?'jugador-puntos-media-cambio':'jugador-puntos-media'}>{jugador.puntos_media}</p>
-            <p className={jugadoresPropios?'jugador-puntos-totales-cambio':'jugador-puntos-totales'}>{jugador.puntos_totales}</p>
-            {jugadoresPropios && <div className='container-button-cambio'><button className='cambio-jugador'><img src={svgIconCambio} style={{height: 15, width: 15, justifyContent: 'center', alignItems: 'center'}}/></button></div>}
-          </div>
-        )
-        })}
+          return(
+            <>
+              <div className='jugador' style={{borderLeft: jugador.posicion==1?'5px solid #ffc107':jugador.posicion==2?'5px solid #2196f3':jugador.posicion==3?'5px solid #4caf50':'5px solid #f44336', 
+                borderBottom: index==10?'none':'1px solid #ccc', borderRadius: index==10? '0px 0px 0px 5px':'0px 0px 0px 0px',
+                borderTop: index==0?'1px solid #ccc':'none', color: jugadoresPropios?'black':'dimgray'}}key={index}>
+                <p className='jugador-nombre' onClick={()=> goJugador(jugador)}>{jugador.nombre}</p>
+                <div className='jugador-equipo'>
+                  <img  src={'/'+jugador.path_foto+'.png'} style={{width: 25, height: 25, marginTop: 5}}/>
+                </div>
+                <div className={jugadoresPropios?'jugador-puntos-media-cambio':'jugador-puntos-media'} style={{display: 'flex', flexDirection: 'row', justifyContent: 'end'}}>
+                                      {jugador.goles!=0 && [...Array(jugador.goles)].map((_, idx) => (
+                                        <img src={svgBallFootball} style={{height: 15, width: 15, marginTop: 11, marginLeft: 4}}/>
+                                      ))}
+                                      <p className={jugPropios?'jugador-puntos-jornada-cambio':'jugador-puntos-jornada'} style={{color: jugador.puntos_jornada<0?'#f44336':jugador.puntos_jornada<1?'#78909c'
+                                        :jugador.puntos_jornada<5?'#ff9800':jugador.puntos_jornada<10?'#4caf50':'#2196f3', fontWeight: 600}}>{jugador.puntos_jornada}</p>
+                                    </div>
+                <p className={jugadoresPropios?'jugador-puntos-media-cambio':'jugador-puntos-media'}>{jugador.puntos_media}</p>
+                <p className={jugadoresPropios?'jugador-puntos-totales-cambio':'jugador-puntos-totales'}>{jugador.puntos_totales}</p>
+                {jugadoresPropios && <div className='container-button-cambio'><button className='cambio-jugador' onClick={(() => cambiarJugador(jugador))}>
+                    <img src={arrowDown} style={{height: 15, width: 15, justifyContent: 'center', alignItems: 'center'}}/>
+                  </button></div>}
+              </div>
+              {mostrarSuplentes && jugadorCambioActual == jugador.id_jugador && suplentes.map((suplente) => {
+                return(
+                  <div className='jugador' style={{borderLeft: '5px solid #ccc', 
+                    borderBottom: index==10?'none':'2px solid #ccc', borderRadius: index==10? '0px 0px 0px 5px':'0px 0px 0px 0px', backgroundColor: 'gainsboro',
+                    borderTop: index==0?'1px solid #ccc':'none', color: jugadoresPropios?'gray':'dimgray', borderRight: '1px solid #ccc'}}key={index}>
+                    <p className='jugador-nombre' onClick={()=> goJugador(suplente)}>{suplente.nombre}</p>
+                    <div className='jugador-equipo'>
+                      <img  src={'/'+suplente.path_foto+'.png'} style={{width: 25, height: 25, marginTop: 5}}/>
+                    </div>
+                    <div className={jugadoresPropios?'jugador-puntos-media-cambio':'jugador-puntos-media'} style={{display: 'flex', flexDirection: 'row', justifyContent: 'end'}}>
+                                          {suplente.goles!=0 && [...Array(suplente.goles)].map((_, idx) => (
+                                            <img src={svgBallFootball} style={{height: 15, width: 15, marginTop: 11, marginLeft: 4}}/>
+                                          ))}
+                                          <p className={jugPropios?'jugador-puntos-jornada-cambio':'jugador-puntos-jornada'} style={{color: suplente.puntos_jornada<0?'#f44336':suplente.puntos_jornada<1?'#78909c'
+                                            :suplente.puntos_jornada<5?'#ff9800':suplente.puntos_jornada<10?'#4caf50':'#2196f3', fontWeight: 600}}>{suplente.puntos_jornada}</p>
+                                        </div>
+                    <p className={jugadoresPropios?'jugador-puntos-media-cambio':'jugador-puntos-media'}>{suplente.puntos_media}</p>
+                    <p className={jugadoresPropios?'jugador-puntos-totales-cambio':'jugador-puntos-totales'}>{suplente.puntos_totales}</p>
+                    <div className='container-button-cambio'>
+                      <button className='cambio-suplente' onClick={(() => cambiarJugadorSuplente(jugador.id_jugador,suplente.id_jugador))}>
+                        <img src={arrowUp} style={{height: 15, width: 15, justifyContent: 'center', alignItems: 'center'}}/>
+                      </button>
+                    </div>
+                  </div>
+                );
+              })}
+            </>
+          )}) 
+        }
     </div>
   )
 }
